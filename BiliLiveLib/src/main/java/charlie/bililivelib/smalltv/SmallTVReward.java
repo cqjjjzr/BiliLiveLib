98daee584b15d6a928cf02109704bd54f42f724b
CharlieJiang <cqjjjzr@126.com>
2017-01-14 14:08:55 +0800

    SmallTV Fixes!

--- BiliLiveLib/src/main/java/charlie/bililivelib/smalltv/SmallTVReward.java
+++ BiliLiveLib/src/main/java/charlie/bililivelib/smalltv/SmallTVReward.java
@@ -1,29 +1,113 @@
 package charlie.bililivelib.smalltv;
 
+import charlie.bililivelib.util.I18n;
+import com.google.gson.annotations.SerializedName;
+import lombok.AccessLevel;
+import lombok.Data;
 import lombok.Getter;
+import org.jetbrains.annotations.Nullable;
 
-@Getter
-public enum SmallTVReward {
-    SMALL_TV        ("大号小电视", 1),
-    BLUE_WHITE_PANTS("蓝白胖次", 2),
-    B_KELA          ("B坷垃", 3),
-    NYA_NIANG       ("喵娘", 4),
-    BENTO           ("便当", 5),
-    SLIVER          ("银瓜子", 6),
-    HOT_STRIP       ("辣条", 7);
+@Data
+public class SmallTVReward {
+    private static final int STATUS_MISS = 1;
 
-    private String displayName;
-    private int id;
+    /**
+     * code : 0
+     * msg : OK
+     * data : {"fname":"奶酪酪酪酪","sname":"南傾挽风","reward":{"id":7,"num":2},"win":0,"status":0}
+     */
 
-    SmallTVReward(String displayName, int id) {
-        this.displayName = displayName;
-        this.id = id;
+    private int code;
+    @SerializedName("msg")
+    private String message;
+    @Getter(AccessLevel.PRIVATE)
+    private DataBean data;
+
+    public boolean isMiss() {
+        return data.status == STATUS_MISS;
     }
 
-    public static SmallTVReward fromID(int id) {
-        for (SmallTVReward reward : SmallTVReward.values()) {
-            if (reward.getId() == id) return reward;
+    public String getSenderUsername() {
+        return data.senderUsername;
+    }
+
+    public String getFirstPrizeUsername() {
+        return data.prizeUsername;
+    }
+
+    @Nullable
+    public Reward getReward() {
+        return data.reward;
+    }
+
+    public boolean isWon() {
+        return data.win == 0;
+    }
+
+    public int getStatus() {
+        return data.status;
+    }
+
+    private static class DataBean {
+        /**
+         * fname : 奶酪酪酪酪
+         * sname : 南傾挽风
+         * reward : {"id":7,"num":2}
+         * win : 0
+         * status : 0
+         */
+
+        @SerializedName("fname")
+        private String senderUsername;
+        @SerializedName("sname")
+        private String prizeUsername;
+        private Reward reward;
+        private int win;
+        private int status;
+    }
+
+    public static class Reward {
+        /**
+         * id : 7
+         * num : 2
+         */
+
+        @SerializedName("id")
+        private int id;
+        @SerializedName("num")
+        @Getter
+        private int count;
+
+        public Kind getKind() {
+            return Kind.forID(id);
         }
-        return HOT_STRIP;
+
+        public enum Kind {
+            SMALL_TV(1),
+            BLUE_WHITE_PANTS(2),
+            B_KELA(3),
+            NYA_NIANG(4),
+            BENTO(5),
+            SLIVER(6),
+            HOT_STRIP(7),
+            UNKNOWN(-1);
+            @Getter
+            private int id;
+
+            Kind(int id) {
+                this.id = id;
+            }
+
+            public static Kind forID(int id) {
+                for (Kind reward : Kind.values()) {
+                    if (reward.getId() == id) return reward;
+                }
+                return UNKNOWN;
+            }
+
+            public String getDisplayName() {
+                return I18n.getString("smalltv.reward." + name().toLowerCase());
+            }
+        }
     }
 }
