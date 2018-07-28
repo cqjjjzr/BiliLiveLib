c4b98c91923baf16fa17220011b263f1184cbce9
Charlie Jiang <cqjjjzr@126.com>
2018-07-28 15:59:56 +0800

    orz
    Signed-off-by: Charlie Jiang <cqjjjzr@126.com>

--- BiliLiveLib/src/main/java/charlie/bililivelib/BiliLiveLib.java
+++ BiliLiveLib/src/main/java/charlie/bililivelib/BiliLiveLib.java
@@ -9,6 +9,6 @@ package charlie.bililivelib;
 public class BiliLiveLib {
     public static final String PROJECT_NAME = "BiliLiveLib";
 
-    public static final String VERSION = "rv2-SNAPSHOT";
+    public static final String VERSION = "rv2";
     public static String DEFAULT_USER_AGENT = PROJECT_NAME + " " + VERSION;
 }
